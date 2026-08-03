e16195569bdbb1182ade2087e326720955983e06
kevinheyday <kjidiw@hanmail.net>
2026-08-04 07:25:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-voca-shell-v1';
+const CACHE_NAME = 'my-voca-shell-v2';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
